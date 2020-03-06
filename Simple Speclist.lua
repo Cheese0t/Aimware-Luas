4e0b3d2fa4f8336a59bd65a98fa9c109cefc0d67
Cheese0t <cheeseot101@gmail.com>
2020-03-06 15:44:38 +0100

    Temporary fix for GetPlayerInfo crash

--- Simple Speclist.lua
+++ Simple Speclist.lua
@@ -1,54 +1,54 @@
 --SimpleSpeclist by Cheeseot
-local specshit = gui.Reference( "MISC", "GENERAL", "Extra");
-local BetterSpecBox = gui.Checkbox( specshit, "lua_betterspec", "Simple speclist", 0 );
+local specshit = gui.Reference( "MISC", "GENERAL", "Extra")
+local BetterSpecBox = gui.Checkbox( specshit, "lua_betterspec", "Simple speclist", 1 )
+BetterSpecBox:SetDescription("Position based on default spectator window.")
 
 function betterspec()
 local specfont = draw.CreateFont('Vendetta', 20)
 local sorting = 0
-local specpos1, specpos2 = gui.GetValue("spectators");
+local specpos1, specpos2 = gui.GetValue("spectators")
 	if BetterSpecBox:GetValue() then
-	gui.SetValue("misc.showspec", 0);
-	local lp = entities.GetLocalPlayer();
+	gui.SetValue("misc.showspec", 0)
+	local lp = entities.GetLocalPlayer()
 		if lp ~= nil then
-            local players = entities.FindByClass("CCSPlayer");
-        	for i = 1, #players do
-			local player = players[i];
-				if player ~= lp and player:GetHealth() <= 0 then
-				local name = player:GetName();
+			for i, v in ipairs(entities.FindByClass("CCSPlayer")) do
+			local player = v
+				if player ~= lp and not player:IsAlive() then
+				local name = player:GetName()
 					if player:GetPropEntity("m_hObserverTarget") ~= nil then
 					local playerindex = player:GetIndex()
-					local botcheck = client.GetPlayerInfo(playerindex)
-						if (botcheck["IsGOTV"] == false and botcheck["IsBot"] == false) then
+					--local botcheck = client.GetPlayerInfo(playerindex)
+						--if (not botcheck["IsGOTV"] and not botcheck["IsBot"]) then
 						local target = player:GetPropEntity("m_hObserverTarget");
 							if target:IsPlayer() then
-							local targetindex = target:GetIndex();
-							local myindex = client.GetLocalPlayerIndex();
+							local targetindex = target:GetIndex()
+							local myindex = client.GetLocalPlayerIndex()
 								if lp:IsAlive() then
 									if targetindex == myindex then
-									draw.SetFont(specfont);
-									draw.Color(255,255,255,255);
-									draw.Text( specpos1, specpos2 + (sorting * 16), name );
-									draw.TextShadow( specpos1, specpos2 + (sorting * 16), name );
-									sorting = sorting + 1;
+									draw.SetFont(specfont)
+									draw.Color(255,255,255,255)
+									draw.Text( specpos1, specpos2 + (sorting * 16), name )
+									draw.TextShadow( specpos1, specpos2 + (sorting * 16), name )
+									sorting = sorting + 1
 									end
 								end
 									
-								if lp:GetHealth() <= 0 then
+								if not lp:IsAlive() then
 									if lp:GetPropEntity("m_hObserverTarget") ~= nil then
-									local myspec = lp:GetPropEntity("m_hObserverTarget");
+									local myspec = lp:GetPropEntity("m_hObserverTarget")
 									local myspecindex = myspec:GetIndex()
 									if targetindex == myspecindex then
-									draw.SetFont(specfont);
-									draw.Color(255,255,255,255);
-									draw.Text( specpos1, specpos2 + (sorting * 16), name );
-									draw.TextShadow( specpos1, specpos2 + (sorting * 16), name );
-									sorting = sorting + 1;
+									draw.SetFont(specfont)
+									draw.Color(255,255,255,255)
+									draw.Text( specpos1, specpos2 + (sorting * 16), name )
+									draw.TextShadow( specpos1, specpos2 + (sorting * 16), name )
+									sorting = sorting + 1
 									end
 								end
 								
 								end
 							end
-						end
+						--end
 					end
 				end
 			end
