063ad1e2c3ca9b46fdfd83a474ee8609b6958827
Cheese0t <cheeseot101@gmail.com>
2020-04-16 13:16:36 +0200

    last one

--- AdvancedChams/AdvancedChams.lua
+++ AdvancedChams/AdvancedChams.lua
@@ -28,7 +28,7 @@ local function handleUpdates()
 			if fadeout < 0 then fadeout = 0 end
 			if fadeout > 255 then fadeout = 255 end
 		end
-		if updateframes > 0.25 then fadein = 650 end
+		if updateframes >= 0.25 then fadein = 650 end
 
 		for i = 0, 650 do
 			local alpha = 200-i/3 - fadeout
